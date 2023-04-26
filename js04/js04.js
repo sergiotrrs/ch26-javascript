bdcaabf98d89f4a6251eff1cfb1ef9b8c8059f1f
Sergio Torres <sergio.torres@idr.edu.mx>
2023-04-26 14:33:03 -0600

    feat: sesion js04 arreglos y ciclos#

---
+++ js04/js04.js
@@ -0,0 +1,16 @@
+console.log("Sesión Js04 Arreglos y ciclos");
+
+// Declarar un array
+ const nombresCh26 = ["Sofi", "Alejandro", "Ixchel", "Said"];
+ // Usando el constructor de arreglos
+ const apellidosCh26 = new Array("Montellano", "Torres", "Vazquez");
+ const numCajonesEstacionamiento = new Array(4); // [ , , , ]
+ const numCohortesActivas = new Array(26,27,28,29); // [26 , 27 , 28 , 29 ]
+
+
+ console.log("Apellido de Sergio " + apellidosCh26[1] );
+
+ // Agregar un nuevo apellido al final del arreglo apellidos
+ apellidosCh26.push("Ruiz");
+ console.log(apellidosCh26)
+
