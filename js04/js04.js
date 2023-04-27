ee8719d2b2428fed867b0cc3de8ee3f11db0734e
Sergio Torres <sergio.torres@idr.edu.mx>
2023-04-27 16:53:49 -0600

    ci: fifo y lifo

--- js04/js04.js
+++ js04/js04.js
@@ -21,7 +21,7 @@ console.log("Sesión Js04 Arreglos y ciclos");
 const personasEnCohorte = [ 
 /*ch26*/    ["Arath"  , "José"   , "Karen"     , "Sofi"          ], 
 /*ch27*/    ["Manuel" , "María"  , "Francisco" , "Pablo", "Pepe" ], 
-/*ch28*/    ["Doris"  , "Brendan", "Lucas"                       ] 
+/*ch28*/    ["Doris"  , "Brendan", "Lucas"     , "Maria"         ] 
 ]; 
 
 console.log("Personas ch27: " + personasEnCohorte[1].join(", ") );
@@ -42,7 +42,7 @@ console.log("Personas ch27: " + personasEnCohorte[1].join(", ") );
 // myArray3D = [ [ [],[],[]  ], [ [],[],[] ] ];  myArray3D[][][];
 
 // Iterar todos los elementos en un arreglo de 2 dimensiones.
-/*
+
 for (let fila = 0; fila < personasEnCohorte.length; fila++) {
     console.log(`Indice ${fila}: ${personasEnCohorte[fila]}`)
     for (let columna = 0; columna <  personasEnCohorte[fila].length; columna++) {
@@ -50,6 +50,163 @@ for (let fila = 0; fila < personasEnCohorte.length; fila++) {
         
     }
 }
-*/
+
 // Usando forEach
 personasEnCohorte.forEach( fila => fila.forEach( columna =>  console.log(columna) ));
+
+// For of
+// Ejecuta sentencias por cada elemento de un objeto iterable(arrays, strings y objetos )
+
+const myName = "Sergio";
+for (let char of myName){
+    console.log(char);
+}
+
+for (let fila of personasEnCohorte){
+    for (let columna of fila){
+        console.log(columna);
+    }
+}
+
+// -------------------- Consideraciones para el ciclo For -------------------------------
+// No es necesario indicar el inicio, la comparación y la expresion final
+/*
+Sintaxis ciclo For:
+   for (inicio; comparación; expresiónFinal  ){
+    sentencias;
+   }
+*/
+// Break: Rompe con la iteración en curso y sale del ciclo
+let nIteracion = 0;
+for( /*inicio*/  ;  /*comparacion*/   ;  /*ExpFinal*/ ){
+    console.log(nIteracion);
+    if(nIteracion === 5) break;
+    nIteracion++;
+}
+
+// -------------------- Ejercicio ---------------------------
+// Iterar y mostrar a las personas en personasEnCohorte y detener cuando se encuentre a María.
+// Dejar de iterar
+personas: // Se usa un label para indicar en el break hacia que ciclo romperá.
+for (let fila of personasEnCohorte){
+    columna:
+    for (let columna of fila){
+        console.log(columna);
+        if (columna === "María") break personas; // Se indica el label que rompe
+    }
+    //    if(columna == "María") break; // Si no se usa el label, se tiene que realizar esto
+}
+
+// Continue
+/*
+ Termina la ejecución de la sentencia en la iteración actual y continua la ejecución del ciclo
+ con la próxima iteración (o en el label que se indique).
+*/
+console.log("==== uso de continue======")
+// No imprimir a María
+personas: 
+for (let fila of personasEnCohorte){
+    for (let columna of fila){
+        if (columna === "María" ||  columna === "Maria") continue; 
+        
+        const concatenacion = "Persona " + columna;
+        console.log(concatenacion);
+        
+    }
+}
+
+// ------------------ Ciclo While ---------------------------------
+/*
+ Crea un bucle que ejecuta una sentencia mientras la condición especificada sea verdadera.
+
+ Sintaxis:
+
+while( condición )
+ sentencias;
+
+*/
+/*
+let counter = 0;
+while ( confirm("¿Quieres continuar?")  ){
+    console.log(`Has continuado ${++counter} ${counter>1? "veces":"vez"}`);
+}
+*/
+
+const numbers = [9,5,6,7];
+//numbers.forEach( number => console.log(number));
+
+let i =0;
+while(i<numbers.length){
+    console.log(numbers[i],)
+    i++;
+}
+
+// ---------------- Práctica -----------------------
+/*
+ 
+
+*/
+
+// ------------- Ciclo do while --------------------
+/*
+ Crea un bucle que ejecuta una sentencia hasta que la condición de comprobación se evalue
+ como falso. La condición se evalua después de ejecutar la sentencia, por lo que la sentencia
+ se ejecuta por lo menos una vez.
+
+ Sintaxis:
+
+  do{
+    setencias;
+  } while( comprobación );
+
+
+
+*/
+let iteration = 0; 
+/*
+do {
+    iteration++;
+    console.log("N. iteración do-while: " + iteration);
+
+} while( confirm("¿Deseas Continuar? ") );
+*/
+
+let valor = -2;
+while ( valor < 5) {
+    console.log("While " + valor );
+    valor ++;
+}
+
+valor = 4;
+do {
+    console.log("Do-While " + valor );
+    valor ++;
+} while ( valor < 5);
+
+/*
+FIFO: first input, first output
+LIFO: last input, first output
+
+*/
+const alimentos = [];
+
+// Usar FIFO:
+alimentos.push("Manzanas del lunes");
+alimentos.push("Manzanas del miércoles");
+console.log( alimentos);
+// Sacar las manzanas
+console.log("Sacando las manzanas antiguas: " + alimentos.shift() );
+console.log( alimentos);
+console.log("Sacando las manzanas antiguas: " + alimentos.shift() );
+console.log( alimentos);
+
+// Usar LIFO
+const productos = [];
+// Temporada navideña
+productos.push("Desarmadores");
+productos.push("Luces de colores");
+console.log(productos);
+console.log("Sacando el producto reciente: " + productos.pop() );
+console.log(productos);
+
+
