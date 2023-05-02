90ba3fb69cad6acf5db25314278aeacff69b6429
Sergio Torres <sergio.torres@idr.edu.mx>
2023-05-02 12:09:44 -0600

    ci: desestructuracion

--- js05/js05.js
+++ js05/js05.js
@@ -101,3 +101,120 @@ console.log( NaN || "El valor es un número que se puede procesar") // "El valor
 console.log( "false" || "El valor es false" ); // "false"
 console.log( "" || "No hay dato" ); // "No hay dato"
 
+const noDefinido = true;
+console.log ( noDefinido ||  theBigBang  );
+
+// Quiero imprimir "Estás Autorizado" si la variable isOnline y isActive son true.
+const isOnline = true;
+const isActive = true;
+if ( isOnline )
+  if( isActive )
+    console.log("Estás Autorizado");
+
+if (isOnline && isActive)
+  console.log('Estas autorizado')
+
+console.log(isOnline && isActive ? "Estas autorizado": "No estas autorizado");
+
+console.log( isOnline && isActive && "Estas autorizado" );
+
+// --------------------- Operadores relacionales ---------------------------
+/*
+Un operador de comparación compara sus operandos y devuelve un valor lógico en función de si la comparación es verdadera (true) o falsa (false). Los operandos pueden ser valores numéricos, de cadena, lógicos u objetos. Las cadenas se comparan según el orden lexicográfico estándar, utilizando valores Unicode. En la mayoría de los casos, si los dos operandos no son del mismo tipo, JavaScript intenta convertirlos a un tipo apropiado para la comparación. Este comportamiento generalmente resulta en comparar los operandos numéricamente.
+
+< menor que
+> mayor que
+<= menor o igual que
+>= mayor o igual que
+== igual que
+
+*/
+
+console.log( true == "true"  ); // false
+console.log(  23 < "40" ); // true
+
+console.log(  "b" < "A" ); // false ( 98  < 65 )
+
+console.log(  "Hola" < "HOLA"); // false
+          
+console.log(  "Hola" < "123"); // false 
+          //  H: 72     1: 49
+
+console.log(  "Hola" < "KeHace" + 1000 ); // true 
+           // "Hola" < "KeHace1000"  
+
+console.log(  "23" == 23  ); // true
+
+// ------------------- Comparación estricta ------------------
+/*
+ === estrictamente igual, debe ser igual en el valor y tipo
+ !== estrictamente diferente, debe ser difente en valor y tipo
+
+*/
+      
+console.log( "23" === 23  ); // false;
+console.log( "true" === true ); // false;
+console.log( "45" !== 45  ) // true
+
+
+// Operador se asignación =
+const myNumber = 34; 
+
+// Desestructuración (Destructuring)
+/*
+ Es una expresión en JS que hace posible extraer datos de arreglos o objetos y nos
+ ayuda a realizar asignaciones más complejas.
+*/
+let dataA = 10;
+let dataB = 20;
+console.log(`a: ${dataA} , b:${dataB} `);
+// Quiero intercambiar los datos
+/*
+let temporal;
+temporal = dataA;
+dataA = dataB;
+dataB = temporal;
+*/
+[ dataA, dataB  ] = [ dataB, dataA ];
+console.log(`a: ${dataA} , b:${dataB} `);
+
+const calificaciones = [50, 80, 100, 30, 60, 99 ];
+// dataA = calificaciones[0]; // 50
+// dataB = calificaciones[1]; // 80
+let resto; // rest parameter
+[ dataA, , , dataB, ...resto  ] = calificaciones; 
+console.log(`a: ${dataA} , b:${dataB} `);
+console.log(resto);
+
+const primerAnio ={
+  dataA: "43 alumnos",
+  dataB: "50 alumnos",
+  dataC: "20 alumnos"
+};
+( {dataA, dataB } = primerAnio);
+
+console.log(`a: ${dataA} , b:${dataB} `);
+
+const auto = {
+  model: "Chevrolet",
+  year: 2023,
+  color : "red",
+  motor: "2.0"
+}
+
+console.log("Modelo " + auto.model ); //auto["model"]
+
+// Asignando el valor model del objeto auto a la constante model
+                  // inicializar con un valor por default
+//const { model, colorAuto = "Pistache"} = auto;
+                  // Asignar un nuevo nombre al atributo
+const { model, color: colorAuto } = auto;
+console.log("Modelo " + model + ", color: " + colorAuto ); 
+
+// ------------------  ejercicio ------------------------
+/*
+   Hacer una función que realice la división de 2 números
+   y me entregue (return) en entero el cociente y residuo
+*/
+
+
