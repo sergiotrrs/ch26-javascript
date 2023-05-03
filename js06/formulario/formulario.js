36d5a0ade3526645172aecc55c1d85292ed5edc3
Sergio Torres <sergio.torres@idr.edu.mx>
2023-05-03 16:47:32 -0600

    ci: class d-none

--- js06/formulario/formulario.js
+++ js06/formulario/formulario.js
@@ -2,9 +2,6 @@ console.log("Sesion JS06-Formulario");
 
 const refForm = document.forms["registro"];
 
-
-
-
 refForm.addEventListener('submit' , (event) =>{
     event.preventDefault(); // omita las acciones por default
     //console.log(event);
@@ -12,8 +9,9 @@ refForm.addEventListener('submit' , (event) =>{
     console.log(email.value);
     // Crear el objeto con los datos del formulario
     user = {
-        email:"",
-        password: "",
+        email: refForm.elements["inputEmail4"].value || "sin email",
+        password:  refForm.elements["inputPassword4"].value,
+        checkbox:  refForm.elements["gridCheck"].checked,
 
     }
     console.log(user);
