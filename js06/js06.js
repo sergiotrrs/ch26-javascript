903bfec4853fc32424add15b42e62cba69565617
Sergio Torres <sergio.torres@idr.edu.mx>
2023-05-03 12:13:43 -0600

    ci: modificaciones al DOM

--- js06/js06.js
+++ js06/js06.js
@@ -1,1 +1,46 @@
-console.log("Sesion Js06")
+console.log("Sesion JS06");
+
+// función definida
+function findElementByIdFnc01 ( /*parametros*/ ){
+    /* instrucciones */
+    return 3*4;
+}
+
+// función expresada
+const findElementByIdFnc02 = function ( /*parámetros*/) {
+    return 3*4;
+};
+
+const findElementByIdFnc03 = (/*parámetros*/ ) => {
+    return 3*4;
+};
+
+const findElementByIdFnc04 = () =>  3*4;
+
+const findElementByIdFnc05 = variableParametro =>  3*4;
+
+const findElementById = ( nameOfDeveloper ) => {
+    const tituloRef = document.getElementById("titulo");
+
+    //tituloRef.innerHTML = tituloRef.innerHTML + nameOfDeveloper;
+    tituloRef.innerHTML += nameOfDeveloper;
+
+}
+findElementById(", Sergio");
+
+const changeSubtitleById = ( newTxt ) => {
+    const subtitleRef = document.getElementById("subtitulo");
+    
+    subtitleRef.innerHTML = ` ${subtitleRef.innerText}, <em>${newTxt}</em>`;
+
+}
+changeSubtitleById("viernes de cholos");
+
+//------------ Encontrar elementos por su tag ----------------------------------
+// getElementsByTagName()
+
+const changeEmphasis = ( newTxt ) => {
+
+};
+
+changeEmphasis("viernes friki");
