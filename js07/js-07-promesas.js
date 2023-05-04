6e4e4a9cc5ce69e791a213e19b60b15e1ba6954c
Sergio Torres <sergio.torres@idr.edu.mx>
2023-05-04 13:27:28 -0600

    division con parametros

--- js07/js-07-promesas.js
+++ js07/js-07-promesas.js
@@ -29,3 +29,69 @@ pinkiPromise
     .catch( ( error )=> console.error("Tu promesa fue rechazada " + error)  );
 
 console.log( "Después de consumir la promesa" );
+
+
+// ----------------- Una promesa que reciba dos números a y b y los divida
+// en caso de que b sea cero, me envíe un rechazo diciéndome por que no se
+// puede realizar la división.
+
+const division = new Promise ( (fncCallBackResolve, fncCallBackReject )=> {
+    const a = 10;
+    const b = 5;
+
+    if(b === 0){
+        fncCallBackReject( "No se puede dividir por cero" )
+    }else{
+        fncCallBackResolve( a/b )
+    }
+});
+
+division
+    .then( (response)=> console.log("promesa resuelta, la division es: " + response))
+    .catch( (error) =>console.error("Tu promesa fue rechazada: " + error ));
+
+
+// -------------- Haciendo la divisón con parámetros --------------------------------
+
+const divisionConParametros = (a, b) =>{
+    const myPromise = new Promise(  (resolve, reject ) =>{
+        if( b === 0 )
+            reject("No se puede realizar una división por cero");
+        else
+            resolve(a/b);
+    } );
+
+    return myPromise;
+}
+
+divisionConParametros(3,4)
+    .then( (response)=> console.log(response) ) // 0.75
+    .catch( (error)=> console.log(error) );
+
+divisionConParametros(3,0)
+    .then( (response)=> console.log(response) )
+    .catch( (error)=> console.log(error) ); // No se puede realizar una división por cero
+
+
+/* 
+
+const divisionAsincrona = (a,b) => {
+    const myPromise =  new Promise( (resolve, reject) =>{
+        if (b!== 0)
+            resolve(a/b);
+        else
+            reject("No se puede realizar una divisón por cero");
+    }  )
+return myPromise;
+}
+
+const divide = (a, b) => {
+  divisionAsincrona(a,b)
+    .then( resolve => console.log(resolve)   )
+    .catch(error => console.error(error) )
+};
+
+divide(3,4);
+divide(3,0);
+
+ */
