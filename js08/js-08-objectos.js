9a693633fa8cf55328e96bf314fe26cba9ce8a0b
Sergio Torres <sergio.torres@idr.edu.mx>
2023-05-05 13:46:08 -0600

    ci: uso de symbol

--- js08/js-08-objectos.js
+++ js08/js-08-objectos.js
@@ -1,1 +1,68 @@
 console.log("Js 08 objetos");
+
+// ----- Crear un objeto en js -------------
+const frutasColores = {
+    // clave: valor
+    fresa: ["roja"],
+    pepino: ["verde"],
+    mango: ["amarillo"],
+    naranja: ["anaranjada", "amarilla"],
+    mandarina: ["anaranjado"],
+    mora: ["morada"],
+    pitaya : ["rosa", "rojo", "blanco", "amarillo"]
+};
+
+// -------- Formas de leer un objeto -----------
+// Leer el color del pepino
+console.log("El pepino es " + frutasColores.pepino );
+// Leer el color de la pitaya
+console.log("La pitaya es " + frutasColores.pitaya[1] ); // rojo
+console.log("La pitaya es " + frutasColores.pitaya.join("-") ); // rosa-rojo-blanco-amarillo
+
+// Modificamos un valor
+// Agregar el color verde al mango
+frutasColores.mango.push("verde");
+// frutasColores.mango = "verde";
+console.log("El mango es " + frutasColores.mango );
+console.log("El mango es " + frutasColores["mango"] );
+
+
+// -------------Iterando los objetos ----------------------------------
+// Iterar los valores con for-in
+// iterar sobre todas las propiedades (keys) de un objeto
+
+for(let fruta in frutasColores){ // fruta tendrá las claves
+    console.log( `${fruta} tiene los colores ${frutasColores[fruta] } ` );
+}
+
+// ------------- Uso  de  symbol --------------------------------------
+/*
+ Se utiliza para añadir claves de propiedades únicas y que estén ocultas
+ a otros mecanismos como la iteración y stringify.
+
+ Esto nos sirve para realizar el concepto de encapsulación.
+*/
+const sueldoOculto = Symbol(); // 0x73783457845
+let variableX = 23;
+
+const datosEmpleado = {
+    nombre : "El kesa",
+    apellido: "Britas",
+    [sueldoOculto]: 50_000,
+    [variableX]: 666, // 23 : 666
+    2 : 12
+}
+
+for (let dato in datosEmpleado){
+    console.log(`${dato}: ${datosEmpleado[dato]}`)
+}
+
+console.log(datosEmpleado["2"] ); // 12
+console.log(datosEmpleado["23"] ); // 666
+console.log(datosEmpleado[variableX] ); // 666
+variableX = 1000;
+console.log(datosEmpleado[variableX] ); // undefined
+
+//localStorage.setItem("datos-empleado",  JSON.stringify( datosEmpleado ));
+sessionStorage.setItem("datos-empleado",  JSON.stringify( datosEmpleado ));
+
