f57f2ff0ebc05a9ee2af60de0540c2183cf03bbd
Sergio Torres <sergio.torres@idr.edu.mx>
2023-05-08 12:21:51 -0600

    ci: usando axios con async-await

--- js09/assets/scripts/index.js
+++ js09/assets/scripts/index.js
@@ -1,1 +1,36 @@
 console.log("JS09 uso de Axios");
+
+const url = "https://reqres.in/api/unknown/23";
+
+const getUsersFetch = () => {
+  fetch(url)
+    .then((response) => response.json())
+    .then((users) => {
+      console.log(users.data);
+    })
+    .catch((error) => console.log(error));
+};
+
+const getUsersAxios = () => {
+  axios
+    .get(url)
+    .then((users) => {
+      console.log(users.data.data);
+    })
+    .catch((error) => console.log(error));
+};
+
+// --------- Usar Axios con async-await --------------
+
+const getUsersAxiosAwait = async () => {
+  try {
+    const users = await axios.get(url);
+    console.log(users);
+  } catch (error) {
+    console.log(error );
+    //alert( error.message  )
+  }
+};
+
+// ------- Solicutd POst con Axios. ------------------
+
