673411da88aca0e42c48268caa2b690464ef0d58
Sergio Torres <sergio.torres@idr.edu.mx>
2023-05-31 11:29:23 -0600

    ci: LIKE, AND, OR, NOT

---
+++ MySQL03/Aleverse.sql
@@ -0,0 +1,12 @@
+INSERT INTO `customers` ( `firstname`, `lastname`, `address`, `email`) 
+	VALUES ( 'Alejandro', 'Lopez', 'Av. México', 'ivan@gamil.com');
+INSERT INTO customers(firstname, lastname, address, email) 
+	VALUES ("Alejandro", "PerGo", "Coyuca Gustavo A. Madero", "assasinsky@hotmail.com");
+INSERT INTO customers(firstname, lastname, address, email) 
+	VALUES ("Alejandra", "GalBe", "Amistad 13", "alejagb@hotmail.com");
+INSERT INTO `customers` (firstname, lastname, address, email)
+	VALUES ("Alejandro", "Nolasco", "Av. Central 113", "alekzss@outlook.com" );
+INSERT INTO `customers` (`firstname`, `lastname`, `address`, `email`) 
+	VALUES ('Alejandro', 'Gomez', 'Calle independencia', 'alexgc@gamil.com');
+INSERT INTO customers (`firstname`, `lastname`, `address`, `email`)
+	VALUES ("Alejandra", "Chaparro", "Toluca", "ale_pato@gmail.com");
--- MySQL03/SELECT y WHERE.sql
+++ MySQL03/SELECT y WHERE.sql
@@ -19,5 +19,47 @@ SELECT COUNT(DISTINCT product_id) AS "N. items vendidos"
  FROM orders_has_products;
 
 SELECT * FROM customers;
+-- Obtener el numero de nombres distintos.
+SELECT COUNT(DISTINCT firstname) FROM customers; -- 8
+
+-- Limitar el número de elementos consultados
+SELECT * FROM customers LIMIT 5;
+
+-- Filtrar (WHERE) solo las personas con el nombre "Alejandro"
+SELECT * FROM customers WHERE firstname = "Alejandro";
+-- Número de personas que se llaman "Alejandro"
+SELECT COUNT(firstname) AS "N. de Alejandro"
+	FROM customers 
+    WHERE firstname = "Alejandro";
+
+-- Filtrar las personas con nombre "Alejandra" o "Alejandro"
+SELECT * FROM `customers` 
+	WHERE `firstname`= 'Alejandro' 
+    OR `firstname`= 'Alejandra';
+SELECT * FROM customers
+	WHERE firstname LIKE "Alejandr_"; -- un guión bajo = 1 carácter
+									 -- un % = múltiples carácteres
+SELECT * FROM customers
+	WHERE firstname IN ("Alejandro", "Alejandra");
+-- Contar el número de personas que tengan el correo @gmail.com
+SELECT COUNT(email) 
+	FROM customers 
+    WHERE email LIKE "%@gmail.com";
+
+-- Operadores lógicos:  AND(&&), OR(||), NOT(!=)
+SELECT * FROM `customers` 
+	WHERE `firstname`= 'Alejandro' || `firstname`= 'Alejandra';
+-- Listas los nombres que NO sean "Alejandro"
+SELECT * FROM customers WHERE firstname != "Alejandro";
+SELECT * FROM customers WHERE NOT firstname = 'alejandro';
+-- Listar los nombres != Alejandro y Alejandra
+SELECT * FROM customers 
+	WHERE NOT firstname LIKE "Alejandr_";
+
+
+
+
+
+
 
        
